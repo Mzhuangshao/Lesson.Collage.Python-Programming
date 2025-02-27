fd506dcff4971952b937c14b6d537687f9583436
M_zhuangshao <79238849+Mzhuangshao@users.noreply.github.com>
2025-02-27 20:54:18 +0800

    upload

--- 00.class/new.py
+++ 00.class/new.py
@@ -1,1 +1,61 @@
-print ("Hello")
+a = 10
+b = 20
+c = a + b
+print( a, "+", b, "=", c)
+
+a = 50
+b = 10
+c = a - b
+print(a, "-", b, "=", c)
+
+a = 100
+b = 50
+c = a * b
+print(a, "*", b, "=", c)
+
+a = 20
+b = 4
+c = a / b
+print(a, "/", b, "=", c)
+
+a = 10
+b = 3
+c = a % b
+print(a, "%", b, "=", c)
+
+a = 10
+b = 5
+c = a ** b
+print(a, "**", b, "=", c)     # 10^5 = 100000    
+
+a = 10
+b = 3
+c = a // b
+print(a, "//", b, "=", c)     # 10//3 = 3
+
+a = 10
+b = 3
+c = a & b
+print(a, "&", b, "=", c)     # 10&3 = 2
+
+a = 10
+b = 3
+c = a | b
+print(a, "|", b, "=", c)     # 10|3 = 11
+
+a = 10
+b = 3
+c = a ^ b
+print(a, "^", b, "=", c)     # 10^3 = 9
+
+a = 10
+b = 3
+c = a << b
+print(a, "<<", b, "=", c)     # 10<<3 = 80
+
+a = 10
+b = 3
+c = a >> b
+print(a, ">>", b, "=", c)     # 10>>3 = 1
+
+# The above code is a basic example of arithmetic operations in Python.
