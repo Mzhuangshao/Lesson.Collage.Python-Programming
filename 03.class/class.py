bf412241c4045e49b453ad34fad1c5f553359fad
M_zhuangshao <79238849+Mzhuangshao@users.noreply.github.com>
2025-03-17 11:25:59 +0800

    upload

---
+++ 03.class/class.py
@@ -0,0 +1,11 @@
+a = 3
+b = -2
+a += b
+print(a)
+
+if a > 0:
+    print("a is positive")
+else:
+    print("a is negative")
+    if a == 0:
+        print("a is zero")
---
+++ 03.class/class3.py
@@ -0,0 +1,4 @@
+result = 0
+for i in range(101):
+    result = result + i
+print(result)
