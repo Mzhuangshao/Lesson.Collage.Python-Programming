181819ea663be4978cde817d460c8f062946fdcd
M_zhuangshao <79238849+Mzhuangshao@users.noreply.github.com>
2025-03-31 15:27:20 +0800

    Update new.py

--- 05.class/new.py
+++ 05.class/new.py
@@ -1,6 +1,47 @@
-string1 = "子非鱼"
-string2 = "，"
-string3 = "安知鱼之乐"
-result = string1.find("鱼")
-print(result)
-print(string1+string2)
+# string1 = "子非鱼"
+# string2 = "，"
+# string3 = "安知鱼之乐"
+# result = string1.find("鱼")
+# print(result)
+# print(string1+string2)
+# print('\'Hello, world!\'')  # 输出：'Hello, world!'
+
+# print("Hello, world!\nHow are you?")  # 输出：Hello, world!
+#                                         #       How are you?
+
+# print("Hello, world!\tHow are you?")  # 输出：Hello, world!    How are you?
+
+# print("Hello,\b world!")  # 输出：Hello world!
+
+# print("Hello,\f world!")  # 输出：
+#                            # Hello,
+#                            #  world!
+
+# print("A 对应的 ASCII 值为：", ord('A'))  # 输出：A 对应的 ASCII 值为： 65
+
+# print("\x41 为 A 的 ASCII 代码")  # 输出：A 为 A 的 ASCII 代码
+
+# decimal_number = 42
+# binary_number = bin(decimal_number)  # 十进制转换为二进制
+# print('转换为二进制:', binary_number)  # 转换为二进制: 0b101010
+
+# octal_number = oct(decimal_number)  # 十进制转换为八进制
+# print('转换为八进制:', octal_number)  # 转换为八进制: 0o52
+
+# hexadecimal_number = hex(decimal_number)  # 十进制转换为十六进制
+# print('转换为十六进制:', hexadecimal_number) # 转换为十六进制: 0x2a
+
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [6, 7, 8, 9, 10]
+list3 = list1 + list2
+list4 = ['a']
+list5 = list4 * 3
+list6 = list1[1:4]
+list7 = ['p','y','t','h','o','n']
+list8 = list7[::-1]
+
+print(list3)
+print(list5)
+print(list6)
+print(list8)
